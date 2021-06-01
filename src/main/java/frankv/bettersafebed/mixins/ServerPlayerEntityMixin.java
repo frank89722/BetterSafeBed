4c3dbf424f8a62b18e95a5aef9ac0ae2d6b4973d
frank89722 <frank89722@gmail.com>
2021-06-01 11:18:29 +0800

    merge fabric-1.16.5

--- src/main/java/frankv/bettersafebed/mixins/ServerPlayerEntityMixin.java
+++ src/main/java/frankv/bettersafebed/mixins/ServerPlayerEntityMixin.java
@@ -1,39 +1,21 @@
 package frankv.bettersafebed.mixins;
 
 import java.util.List;
-import java.util.Optional;
 
-import frankv.bettersafebed.MixinCallbacks;
+import net.minecraft.entity.monster.MonsterEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-
-import com.mojang.authlib.GameProfile;
-import com.mojang.datafixers.util.Either;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.util.Direction;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.vector.Vector3d;
-import net.minecraft.world.World;
 
 @Mixin(ServerPlayerEntity.class)
-public abstract class ServerPlayerEntityMixin extends PlayerEntity {
-	private ServerPlayerEntityMixin(World world, BlockPos pos, float f, GameProfile profile) {
-		super(world, pos, f, profile);
-	}
-
-	@Inject(method="startSleepInBed", locals=LocalCapture.CAPTURE_FAILEXCEPTION, at = @At(value="INVOKE_ASSIGN", target="net/minecraft/world/World.getEntitiesOfClass (Ljava/lang/Class;Lnet/minecraft/util/math/AxisAlignedBB;Ljava/util/function/Predicate;)Ljava/util/List;", ordinal=0))
-	private void onTrySleep(BlockPos at, CallbackInfoReturnable<Either> cir, Optional optAt, SleepResult ret, Direction direction, double d0, double d1, Vector3d vector3d, List list) {
-		MixinCallbacks.onTrySleep(list, (PlayerEntity) this.getEntity());
-	}
-
-	@ModifyVariable(method = "startSleepInBed", at = @At(value="INVOKE_ASSIGN", target="net/minecraft/world/World.getEntitiesOfClass (Ljava/lang/Class;Lnet/minecraft/util/math/AxisAlignedBB;Ljava/util/function/Predicate;)Ljava/util/List;", ordinal = 0))
-	private List injected(List list) {
-		return MixinCallbacks.getList();
+public abstract class ServerPlayerEntityMixin {
+	@ModifyVariable(method = "startSleepInBed", at = @At(value="INVOKE_ASSIGN", target="net/minecraft/world/World.getEntitiesOfClass (Ljava/lang/Class;Lnet/minecraft/util/math/AxisAlignedBB;Ljava/util/function/Predicate;)Ljava/util/List;"))
+	private List additionalCheck(List<MonsterEntity> list) {
+		list.removeIf(m -> !(m.getTarget() instanceof PlayerEntity)
+				||  m.getTarget().getVehicle() != ((PlayerEntity)(Object)this).getVehicle());
+		return list;
 	}
 }
