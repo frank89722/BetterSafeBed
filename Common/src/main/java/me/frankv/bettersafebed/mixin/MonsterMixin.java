b5b6fc13e2f39d5fb6b702c3b0fc409b205c613d
frank89722 <mail@frankv.me>
2023-12-10 17:07:33 +0800

    rewrite the main logic

---
+++ Common/src/main/java/me/frankv/bettersafebed/mixin/MonsterMixin.java
@@ -0,0 +1,24 @@
+package me.frankv.bettersafebed.mixin;
+
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Monster.class)
+public class MonsterMixin {
+
+    @Inject(method = "isPreventingPlayerRest", at = @At("HEAD"), cancellable = true)
+    private void injectIsPreventingPlayerRest(Player player, CallbackInfoReturnable<Boolean> cir) {
+        if (((Monster) (Object) this).getTarget() instanceof Player target) {
+            if (target.equals(player)) {
+                cir.setReturnValue(true);
+                return;
+            }
+        }
+        cir.setReturnValue(false);
+    }
+
+}
