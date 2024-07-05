204e161696216217e2ce592733e01f5ca91059c9
frank89722 <mail@frankv.me>
2024-07-05 22:55:15 +0800

    update to mc 1.21

--- neoforge/src/main/java/me/frankv/bettersafebed/BetterSafeBed.java
+++ neoforge/src/main/java/me/frankv/bettersafebed/BetterSafeBed.java
@@ -1,21 +1,8 @@
 package me.frankv.bettersafebed;
 
-
-import net.neoforged.fml.IExtensionPoint;
-import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 
-@Mod(BetterSafeBed.MODID)
+@Mod("bettersafebed")
 public class BetterSafeBed {
-    public static final String MODID = "bettersafebed";
-
-    public BetterSafeBed() {
-        ModLoadingContext.get().registerExtensionPoint(
-                IExtensionPoint.DisplayTest.class,
-                () -> new IExtensionPoint.DisplayTest(
-                        () -> IExtensionPoint.DisplayTest.IGNORESERVERONLY,
-                        (remote, isServer) -> true
-                )
-        );
-    }
+    public BetterSafeBed() {}
 }
